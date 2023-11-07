329529556b7b07d972c81f3955f4e4929baac45d
Depriest43 <depriest43@gmail.com>
2023-11-06 17:37:04 -0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Cis122LabAssignmentGA1;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Cis122LabAssignmentGA1;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
